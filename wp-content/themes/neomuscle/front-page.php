a49998943e97be8c92079b2d648ff7a1f5e55988
Serhii Bubyr <bubyr@codearty.com>
2018-05-29 16:58:29 +0300

    Add instagram module and block

--- wp-content/themes/neomuscle/front-page.php
+++ wp-content/themes/neomuscle/front-page.php
@@ -1,8 +1,18 @@
 <?php get_header();?>
 
-<?php echo do_shortcode('[metaslider id="10"]'); ?>
+<?php echo do_shortcode('[metaslider id="37"]'); ?>
 
 <div style="height:500px;"></div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="section-instagram">
+            <?php if (is_active_sidebar('homepage-instagram-sidebar')): ?>
+                <?php dynamic_sidebar('homepage-instagram-sidebar');?>
+            <?php endif;?>
+        </div>
+    </div>
+</div>
 
 <?php get_footer();?>
 
