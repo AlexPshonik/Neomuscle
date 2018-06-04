0b44b2371086ecf938637a9dd5d8332ea6a254b1
Serhii Bubyr <bubyr@codearty.com>
2018-06-04 16:36:12 +0300

    Add remove styles plugin functiin

--- wp-content/themes/neomuscle/functions.php
+++ wp-content/themes/neomuscle/functions.php
@@ -192,9 +192,7 @@ function change_existing_currency_symbol($currency_symbol, $currency)
     return $currency_symbol;
 }
 
-//
-
-
+// Remove Plugins Styles
 add_action( 'wp_print_styles', 'remove_plugin_scripts_styles', 100 );
 function remove_plugin_scripts_styles() {
     wp_deregister_style( 'dgwt-wcas-style' );
