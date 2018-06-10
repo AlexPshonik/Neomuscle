6b010314079c34335a71f6c472a1e10df9e743ab
Serhii Bubyr <bubyr@codearty.com>
2018-06-10 23:55:06 +0300

    Add promo section and add semibold font and front page scss file

--- wp-content/themes/neomuscle/front-page.php
+++ wp-content/themes/neomuscle/front-page.php
@@ -2,7 +2,57 @@
 
 <!-- <?php echo do_shortcode('[metaslider id="37"]'); ?> -->
 
-<!-- <div style="height:500px;"></div> -->
+<section class="promo section-padding-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="promo-box">
+                    <div class="icon">
+                        <span class="ui-icon-shield"></span>
+                    </div>
+                    <div class="info">
+                        <h6>Оригинальная продукция</h6>
+                        <p>Гарантируем качество и подлинность товара</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="promo-box">
+                    <div class="icon">
+                        <span class="ui-icon-truck"></span>
+                    </div>
+                    <div class="info">
+                        <h6>Бесплатная доставка</h6>
+                        <p>Бесплатная доставка по Украине от 2000 грн</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-lg-3">
+                <div class="promo-box">
+                    <div class="icon">
+                        <span class="ui-icon-wallet"></span>
+                    </div>
+                    <div class="info">
+                        <h6>Гарантия лучших цен</h6>
+                        <p>Мы уверены в своих ценах!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="promo-box last">
+                    <div class="icon">
+                        <span class="ui-icon-support"></span>
+                    </div>
+                    <div class="info">
+                        <h6>Техническая поддержка</h6>
+                        <p>Онлайн поддержка 24/7</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <div class="section-instagram">
     <div class="container">
