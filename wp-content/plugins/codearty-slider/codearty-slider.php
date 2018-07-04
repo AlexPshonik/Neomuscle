7c6af414bfbbdc100a7224ed63fd805794bf6c07
Serhii Bubyr <bubyr@codearty.com>
2018-07-04 19:30:30 +0300

    Add slide url and version plugin up to 1.0.1

--- wp-content/plugins/codearty-slider/codearty-slider.php
+++ wp-content/plugins/codearty-slider/codearty-slider.php
@@ -4,7 +4,7 @@
     Description: Implementation of a slideshow into WordPress
     Author: Serge Bubyr
     Author URI: https://codearty.com
-    Version: 1.0
+    Version: 1.0.1
 */
 
 // Exit if accessed directly
@@ -33,6 +33,52 @@ function ca_init() {
 }
 add_action('init', 'ca_init');
 
+// Add meta boxes for slider link
+function ca_slider_meta_box_markup($object) {
+    wp_nonce_field(basename(__FILE__), "ca-slider-meta-box-nonce");
+
+    ?>
+        <div>
+            <label for="ca-slider-meta-box-url"><?php echo get_site_url(); ?>/</label>
+            <input name="ca-slider-meta-box-url" type="text" style="margin-left: 5px; min-width: 320px;" value="<?php echo get_post_meta($object->ID, "ca-slider-meta-box-url", true); ?>">
+        </div>
+    <?php  
+}
+
+function add_ca_slider_meta_box() {
+    add_meta_box("ca_slider_url", "Ссылка на слайдер", "ca_slider_meta_box_markup", "slider", "advanced", "high", null);
+}
+
+add_action("add_meta_boxes", "add_ca_slider_meta_box");
+
+function save_cs_slider_meta_box($post_id, $post, $update)
+{
+    if (!isset($_POST["ca-slider-meta-box-nonce"]) || !wp_verify_nonce($_POST["ca-slider-meta-box-nonce"], basename(__FILE__)))
+        return $post_id;
+
+    if(!current_user_can("edit_post", $post_id))
+        return $post_id;
+
+    if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+        return $post_id;
+
+    $slug = "slider";
+    if($slug != $post->post_type)
+        return $post_id;
+
+    $meta_box_url_value = "";
+
+    if(isset($_POST["ca-slider-meta-box-url"]))
+    {
+        $meta_box_url_value = $_POST["ca-slider-meta-box-url"];
+    }   
+    update_post_meta($post_id, "ca-slider-meta-box-url", $meta_box_url_value);
+
+}
+
+add_action("save_post", "save_cs_slider_meta_box", 10, 3);
+
+
 // Register Styles
 function ca_slider_register_styles() {
     // register
@@ -57,6 +103,8 @@ function ca_function($type='ca_function') {
         'post_type' => 'slider',
         'posts_per_page' => 5
     );
+    
+
     $result = '<section class="main-slider">';
     $result .= '<div class="owl-carousel owl-theme">';
  
@@ -65,8 +113,14 @@ function ca_function($type='ca_function') {
     while ($loop->have_posts()) {
         $loop->the_post();
  
-        $the_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $type);
-        $result .='<div id="item"><img title="'.get_the_title().'" src="' . $the_url[0] . '"  alt="'.get_the_title().'"/></div>';
+        $slide_url = get_post_meta($post->ID, "ca-slider-meta-box-url", true);
+        $image_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $type);
+
+        $result .= '<div id="item">';
+        $result .= '<a href="'.get_site_url() .'/'. $slide_url . '">';
+        $result .= '<img title="'.get_the_title().'" src="' . $image_url[0] . '"  alt="'.get_the_title().'"/>';
+        $result .= '</a>';
+        $result .= '</div>';
     }
     $result .= '</div>';
     // $result .='<div id = "htmlcaption" class = "nivo-html-caption">';
