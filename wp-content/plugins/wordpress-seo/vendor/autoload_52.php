815cccd3d40d4b359fb02d038634023f744b3fa8
Serge Bubyr <bubyr@codearty.com>
2018-09-08 15:55:03 +0300

    Upgrade Yoast to 8.1.2

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit63dcc7aa5f4f5ecf72f96589dda1ae3d::getLoader();
+return ComposerAutoloaderInit59441168b75001242136fde52183bc77::getLoader();
