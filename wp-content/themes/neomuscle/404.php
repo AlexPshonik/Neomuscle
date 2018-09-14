91396258588b9866715aab7f09b175ca013ff4ff
Serge Bubyr <bubyr@codearty.com>
2018-09-14 15:20:22 +0300

    Stylized 404 page

--- wp-content/themes/neomuscle/404.php
+++ wp-content/themes/neomuscle/404.php
@@ -16,15 +16,14 @@ get_header();
 				<div class="row">
 					<div class="col-12">
 						<section class="error-404 not-found">
-							<div class="error-404-image">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/404-image.jpg"/>
-							</div>
 							<div class="error-404-content">
-								<h3>Сраница не найдена</h3>
 								<h1>Мы сожалеем!</h1>
-								<h2>Похоже у этой страницы день отдыха.</h2>
+								<h2>Похоже у этой страницы выходной :(</h2>
 								<p>Попробуйте панель поиска или зайдите на нашу домашнюю страницу.</p>
 								<a href="<?php echo get_home_url(); ?>" class="button">Вернуться в Neomuscle</a>
+							</div>
+							<div class="error-404-image">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/404-image.jpg"/>
 							</div>
 						</section>
 					</div>
