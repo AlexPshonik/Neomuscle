00d079b6a7dfe8dd2c4bee8558c52efa8bd74e10
SBubyr <bubyr@codearty.com>
2018-10-29 00:07:57 +0200

    Install online chat

--- wp-content/themes/neomuscle/footer.php
+++ wp-content/themes/neomuscle/footer.php
@@ -88,6 +88,12 @@
 
 <?php wp_footer(); ?>
 
+<script data-skip-moving="true">
+  (function(w,d,u){
+    var s=d.createElement('script');s.async=1;s.src=u+'?'+(Date.now()/60000|0);
+    var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+  })(window,document,'https://cdn.bitrix24.ua/b8631521/crm/site_button/loader_3_0rvjm8.js');
+</script>
 </body>
 </html>
 
